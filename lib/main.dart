2639e901747e87772f291e8ccaa8c3e74e581e75
Refsi Maulana <refsimaulanatkr@gmail.com>
2021-04-19 14:59:44 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:mediaquery_flutter/mainpage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(
+
+      ),
+    );
+  }
+}
+
+
